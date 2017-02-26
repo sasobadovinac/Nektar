d21d04130bc751aca2cfdca00a42c083d2f5646b
mengaldo <gianmarco.mengaldo@gmail.com>
2017-02-25 20:37:33 -0800

    isentropic vortex example corrected. FORCE dx corrected for 2D.wq

--- library/SolverUtils/Advection/AdvectionWeakDG.cpp
+++ library/SolverUtils/Advection/AdvectionWeakDG.cpp
@@ -179,7 +179,7 @@ namespace Nektar
                     {
                         for (int i = 0; i < nLocalPts; ++i)
                         {
-                            m_dx[i+physOffset] = 2.0 * (jac[i] / (nLocalPts-1));
+                            m_dx[i+physOffset] = 2.0 * (jac[i] / (nquad0-1));
                             m_gmat[0][i+physOffset] = gmat[0][i];
                             m_gmat[1][i+physOffset] = gmat[1][i];
                             m_gmat[2][i+physOffset] = gmat[2][i];
