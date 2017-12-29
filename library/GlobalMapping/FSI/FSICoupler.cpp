f110b3e60682f433e230a144690dcab465f2d4d5
Douglas Serson <d.serson@gmail.com>
2017-12-29 13:27:07 -0200

    Fix previous commit

--- library/GlobalMapping/FSI/FSICoupler.cpp
+++ library/GlobalMapping/FSI/FSICoupler.cpp
@@ -35,6 +35,12 @@
 
 #include <GlobalMapping/FSI/FSICoupler.h>
 
+#include <MultiRegions/ContField1D.h>
+#include <MultiRegions/ContField2D.h>
+#include <MultiRegions/ContField3D.h>
+#include <MultiRegions/ContField3DHomogeneous1D.h>
+#include <MultiRegions/ContField3DHomogeneous2D.h>
+
 using namespace std;
 
 namespace Nektar
--- library/GlobalMapping/FSI/GeneralCoupler.cpp
+++ library/GlobalMapping/FSI/GeneralCoupler.cpp
@@ -34,11 +34,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <GlobalMapping/FSI/GeneralCoupler.h>
-#include <MultiRegions/ContField1D.h>
-#include <MultiRegions/ContField2D.h>
-#include <MultiRegions/ContField3D.h>
-#include <MultiRegions/ContField3DHomogeneous1D.h>
-#include <MultiRegions/ContField3DHomogeneous2D.h>
 
 namespace Nektar
 {
