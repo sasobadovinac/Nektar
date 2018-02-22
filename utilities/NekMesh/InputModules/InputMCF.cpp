28854a38f26f9831032943f4e14b80d14687ee78
Michael Turner <mturner4350@gmail.com>
2018-02-22 18:58:16 +0000

    fix merge errors in inputmcf.cpp

--- utilities/NekMesh/InputModules/InputMCF.cpp
+++ utilities/NekMesh/InputModules/InputMCF.cpp
@@ -206,6 +206,18 @@ void InputMCF::ParseFile(string nm)
             it         = parameters.find("BndLayerProgression");
             m_blprog   = it != parameters.end() ? it->second : "2.0";
         }
+
+        it = parameters.find("BndLayerAdjustment");
+        if (it != parameters.end())
+        {
+            m_adjust     = true;
+            m_adjustment = it->second;
+        }
+        else
+        {
+            m_adjust = false;
+        }
+
     }
 
     m_naca = false;
@@ -486,26 +498,26 @@ void InputMCF::Process()
         }
     }
 
-<<<<<<< HEAD
-=======
     ////**** Peralign ****////
     if (m_2D && m_periodic.size())
     {
         vector<string> lines;
         boost::split(lines, m_periodic, boost::is_any_of(":"));
 
-        for (auto &il : lines)
+        for (int i = 0; i < lines.size(); i++)
         {
             module = GetModuleFactory().CreateInstance(
                 ModuleKey(eProcessModule, "peralign"), m_mesh);
 
             vector<string> tmp(2);
-            boost::split(tmp, il, boost::is_any_of(","));
+            boost::split(tmp, lines[i], boost::is_any_of(","));
             module->RegisterConfig("surf1", tmp[0]);
             module->RegisterConfig("surf2", tmp[1]);
-        }
->>>>>>> bedd064a1... Missing surf2 in peralign call from InputMCF
 
+            module->SetDefaults();
+            module->Process();
+        }
+    }
 }
 }
 }
