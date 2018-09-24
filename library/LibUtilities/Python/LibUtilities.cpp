89843b5711da4d33a9e560271ffbd4192d7938da
Emilia Juda <eaj14@ic.ac.uk>
2018-09-24 07:45:30 +0100

    Try to wrap NekFactory class

--- library/LibUtilities/Python/LibUtilities.cpp
+++ library/LibUtilities/Python/LibUtilities.cpp
@@ -33,10 +33,19 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <FieldUtils/Field.hpp>
+#include <FieldUtils/Module.h>
 #include <LibUtilities/Python/NekPyConfig.hpp>
 
+using namespace Nektar::FieldUtils;
+
 void export_Basis();
+
+template<typename tKey,        // reference tag (e.g. string, int)
+         typename tBase,       // base class
+         typename... tParam>
 void export_NekFactory();
+
 void export_Points();
 void export_SessionReader();
 void export_ShapeType();
@@ -53,7 +62,7 @@ BOOST_PYTHON_MODULE(_LibUtilities)
 
     export_Basis();
     export_Points();
-    export_NekFactory();
+    export_NekFactory<ModuleKey, Module, FieldSharedPtr>();
     export_SessionReader();
     export_ShapeType();
     export_SharedArray<double>();
