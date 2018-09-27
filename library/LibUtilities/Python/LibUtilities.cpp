28f5d4724cdfe9be78959e4b8ec6588b4f99ba70
Emilia Juda <eaj14@ic.ac.uk>
2018-09-27 18:46:09 +0100

    Clean up NekFactory wrapper

--- library/LibUtilities/Python/LibUtilities.cpp
+++ library/LibUtilities/Python/LibUtilities.cpp
@@ -36,7 +36,6 @@
 #include <LibUtilities/Python/NekPyConfig.hpp>
 
 void export_Basis();
-void export_NekFactory();
 void export_Points();
 void export_SessionReader();
 void export_ShapeType();
@@ -52,9 +51,7 @@ BOOST_PYTHON_MODULE(_LibUtilities)
     np::initialize();
 
     export_Basis();
-    export_Points();
-    export_NekFactory();
-    export_SessionReader();
+    export_Points();export_SessionReader();
     export_ShapeType();
     export_SharedArray<double>();
     export_NekMatrix<double>();
