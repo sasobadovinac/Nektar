437440846f89c3f5cc047a49c4147721fa8bcd3c
Emilia Juda <eaj14@ic.ac.uk>
2018-09-28 17:15:33 +0100

    Drafted Python FieldConverter class

--- utilities/FieldConvert/Python/FieldConvert.py
+++ utilities/FieldConvert/Python/FieldConvert.py
@@ -1,33 +1,83 @@
-import subprocess
+from operator import methodcaller
 import os.path
 
-class Converter:
+class FieldConverter:
 
 	def __init__(self):
-		# to be obtained from cmake configuration
-		self.FieldConvertPath = "/home/emi/FYP/nektar++/build/utilities/FieldConvert/FieldConvert"
-		self.ModuleList = []
-		self.SessionFile = ""
-		self.InputFile = ""
-		self.OutputFile = ""
-		self.OutputFileOverride = False
+		self.sessionFile = ""
+		self.inputFile = ""
+		self.outputFile = ""
 
-	def addSessionFile(self, SessionFile):
-		self.SessionFile = SessionFile
+		self.processModuleList = []
 
-	def addInputFile(self, InputFile):
-		self.InputFile = InputFile
+		self.moduleFactory = "" # instantiate a new NekFactory for modules
+		self.availiableModuleList = [] # get a list of available modules from factory as a list of names
+		self.moduleList = []
 
-	def addOutputFile(self, OutputFile, override=False):
-		self.OutputFileOverride = override
-		if os.path.isfile(OutputFile) and self.OutputFileOverride is False:
-			print("The output file already exists")
-		else:
-			self.OutputFile = OutputFile
+		self.fieldSharedPtr = "" # FieldSharedPtr
+		self.variableMap = ""
+
+	def addSessionFile(self, sessionFile):
+		assert is_file(sessionFile)
+		self.sessionFile = sessionFile
+
+	def addInputFile(self, inputFile):
+		assert is_file(inputFile)
+		self.inputFile = inputFile
+
+	def addOutputFile(self, outputFile):
+		self.outputFile = outputFile
+
+	def addModule(self, moduleName):
+		assert moduleName in self.availiableModuleList
+		self.moduleList.append(moduleName)
+
+	def _checkFiles(self):
+		assert self.sessionFile is not None
+		assert self.inputFile is not None
+		assert self.outputFile is not None
+
+	def _getFileExtenstion(self, fileName):
+		assert '.' in fileName
+		extenstion = fileName.split('.')[-1]
+		return extenstion
+
+	def _createModuleInstance(self, moduleType, moduleName):
+		module = "" # instantiate new module
+		module.SetDefaults()
+		self.moduleList.append(module)
+
+	def _createProcessModuleInstances(self):
+		for processModule in self.processModuleList:
+			self._createModuleInstance(eProcessModule, processModule)
+
+	def _setModulePriorityList(self):
+		self.moduleList = sorted(self.moduleList, key=methodcaller('GetModulePriority'))
+
+	def _constructVariableMap(self):
+		# recreate variable map to input into Process()
+
+	def _runModules(self):
+		for module in self.moduleList:
+			module.Process(self.variableMap)
 
 	def convert(self):
-		p = subprocess.Popen([self.FieldConvertPath, self.SessionFile, self.InputFile, self.OutputFile], stdout=subprocess.PIPE)
-		print p.communicate()
+		self._checkFiles()
+
+		self._createModuleInstance(eInputModule, _getFileExtenstion(self.sessionFile))
+		self._createModuleInstance(eInputModule, _getFileExtenstion(self.inputFile))
+		self._createModuleInstance(eOutputModule, _getFileExtenstion(self.outputFile))
+
+		self._createProcessModuleInstances()
+
+		self._setModulePriorityList()
+
+		self._constructVariableMap()
+
+		self._runModules()
+
+
+
 
 
 def main():
@@ -36,7 +86,7 @@ def main():
     InputFile = "chan3DH1D.fld"
     OutputFile = "chan3DH1D.dat"
 
-    converter = Converter()
+    converter = FieldConverter()
 
     converter.addSessionFile(SessionFile)
     converter.addInputFile(InputFile)
