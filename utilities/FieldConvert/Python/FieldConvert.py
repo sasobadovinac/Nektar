bac0cd065caca9ef8d39b864d0a3535d415f99d6
Emilia Juda <eaj14@ic.ac.uk>
2018-10-21 21:53:51 +0100

    refactored addProcessModule method

--- utilities/FieldConvert/Python/FieldConvert.py
+++ utilities/FieldConvert/Python/FieldConvert.py
@@ -4,33 +4,34 @@ import os.path
 class FieldConverter:
 
 	def __init__(self):
+		self.fieldSharedPtr = "" # FieldSharedPtr
+		self.moduleFactory = "" # instantiate a new NekFactory for modules
+		self.availiableModuleList = [] # get a list of available modules from factory as a list of names
+
 		self.sessionFile = ""
 		self.inputFile = ""
 		self.outputFile = ""
 
-		self.processModuleList = []
-
-		self.moduleFactory = "" # instantiate a new NekFactory for modules
-		self.availiableModuleList = [] # get a list of available modules from factory as a list of names
 		self.moduleList = []
 
-		self.fieldSharedPtr = "" # FieldSharedPtr
 		self.variableMap = ""
 
 	def addSessionFile(self, sessionFile):
 		assert is_file(sessionFile)
 		self.sessionFile = sessionFile
+		self._createModuleInstance(eInputModule, _getFileExtenstion(self.sessionFile))
 
 	def addInputFile(self, inputFile):
 		assert is_file(inputFile)
 		self.inputFile = inputFile
+		self._createModuleInstance(eInputModule, _getFileExtenstion(self.inputFile))
 
 	def addOutputFile(self, outputFile):
 		self.outputFile = outputFile
+		self._createModuleInstance(eOutputModule, _getFileExtenstion(self.outputFile))
 
-	def addModule(self, moduleName):
-		assert moduleName in self.availiableModuleList
-		self.moduleList.append(moduleName)
+	def addProcessModule(self, processModuleName):
+		self._createModuleInstance(eProcessModule, processModuleName)
 
 	def _checkFiles(self):
 		assert self.sessionFile is not None
@@ -43,13 +44,10 @@ class FieldConverter:
 		return extenstion
 
 	def _createModuleInstance(self, moduleType, moduleName):
+		assert moduleName in self.availiableModuleList
 		module = "" # instantiate new module
 		module.SetDefaults()
 		self.moduleList.append(module)
-
-	def _createProcessModuleInstances(self):
-		for processModule in self.processModuleList:
-			self._createModuleInstance(eProcessModule, processModule)
 
 	def _setModulePriorityList(self):
 		self.moduleList = sorted(self.moduleList, key=methodcaller('GetModulePriority'))
@@ -64,20 +62,11 @@ class FieldConverter:
 	def convert(self):
 		self._checkFiles()
 
-		self._createModuleInstance(eInputModule, _getFileExtenstion(self.sessionFile))
-		self._createModuleInstance(eInputModule, _getFileExtenstion(self.inputFile))
-		self._createModuleInstance(eOutputModule, _getFileExtenstion(self.outputFile))
-
-		self._createProcessModuleInstances()
-
 		self._setModulePriorityList()
 
 		self._constructVariableMap()
 
 		self._runModules()
-
-
-
 
 
 def main():
@@ -91,6 +80,8 @@ def main():
     converter.addSessionFile(SessionFile)
     converter.addInputFile(InputFile)
     converter.addOutputFile(OutputFile)
+
+    converter.addProcessModule("vorticity")
 
     converter.convert()
     
