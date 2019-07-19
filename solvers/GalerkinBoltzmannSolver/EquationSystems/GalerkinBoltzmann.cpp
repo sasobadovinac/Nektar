292752f66afecdb1b5b3923c063d3e7dfa616e2f
Yan Zhang <yzhang5@euston.ae.ic.ac.uk>
2019-07-19 11:45:39 +0100

    Updates. Boundary conditions needs consistency

--- solvers/GalerkinBoltzmannSolver/EquationSystems/GalerkinBoltzmann.cpp
+++ solvers/GalerkinBoltzmannSolver/EquationSystems/GalerkinBoltzmann.cpp
@@ -74,6 +74,7 @@ namespace Nektar
         m_tau    = m_session->GetParameter("tau");
         m_traceNormals =  Array<OneD, Array<OneD, NekDouble> >(m_spacedim);
         int nTracePointsTot = m_fields[0]->GetTrace()->GetTotPoints();
+        //int nTracePointsTot = m_fields[0]->GetTraceNpoints();
         for(int i = 0; i < m_spacedim; ++i)
         {
             m_traceNormals[i] = Array<OneD, NekDouble> (nTracePointsTot);
@@ -136,7 +137,6 @@ namespace Nektar
         int ncoeffs = m_fields[0]->GetNcoeffs();
 
         // Calculate inner product with respect to derivative of basis of volume flux
-
         Array<OneD, Array<OneD, NekDouble> > volflux(nFields);
         for(int i = 0; i < nFields; ++i)
         {
@@ -144,7 +144,6 @@ namespace Nektar
         }
         
         EvaluateIProductWRTDerivBaseVolFlux(inarray,volflux);
-        
         // Calculate the normal numerical flux on trace boudaries
         // Add upwind normal flux component
         int nTracePointsTot = m_fields[0]->GetTrace()->GetTotPoints();
@@ -153,7 +152,6 @@ namespace Nektar
         Array<OneD, Array<OneD, NekDouble> > Fwd    (nFields);
         Array<OneD, Array<OneD, NekDouble> > Bwd    (nFields);
         Array<OneD, Array<OneD, NekDouble> > numflux(nFields);
-
         for(int i = 0; i < nFields; ++i)
         {
             Fwd[i]     = Array<OneD, NekDouble>(nTracePointsTot, 0.0);
@@ -163,7 +161,6 @@ namespace Nektar
         }
 
         EvaluateNormalNumFlux(Fwd,Bwd,numflux);
-
         // Project back to physical space
         for(int i = 0; i < nFields; ++i)
         {
@@ -172,16 +169,14 @@ namespace Nektar
             m_fields[i]->MultiplyByElmtInvMass(volflux[i], volflux[i]);
             m_fields[i]->BwdTrans             (volflux[i], outarray[i]);
         }
-
         //-------------------------------------------------------
         // Negate the RHS
-	/* 
         int nq = m_fields[0]->GetTotPoints();
         for (int i = 0; i < nFields; ++i)
         {
             Vmath::Neg(nq, outarray[i], 1);
+            //Vmath::Zero(nq, outarray[i], 1);
         }
-	*/
         AddSourceTerms(inarray,outarray);
        
     }
@@ -278,7 +273,8 @@ namespace Nektar
                                                   Array<OneD, Array<OneD, NekDouble> > &numflux)
     {
         int nTracePointsTot = m_fields[0]->GetTrace()->GetTotPoints();
-
+	NekDouble Interior[6][6]; // matrix V*Lambda^+*V^(-1)  along the normal
+        NekDouble Exterior[6][6]; // matrix V*Lambda^-*V^(-1)  opposite to the normal
         // get hold of normals
 
         NekDouble sqrt2 = sqrt(2);
@@ -287,88 +283,114 @@ namespace Nektar
 
         for(int i = 0 ; i < nTracePointsTot; ++i)
         {
-
             NekDouble nx = m_traceNormals[0][i];
-            
-            if(nx > 0)
-            {
-                numflux[0][i] = (sqrt3*Fwd[0][i] + 3*Fwd[1][i]
-                                 + sqrt6*Fwd[4][i])*nx/6.0 ;
-
-                numflux[1][i] = 0.5*(Fwd[0][i] + sqrt3*Fwd[1][i]
-                                     + sqrt2*Fwd[4][i])*nx;
-                
-                numflux[2][i] = 0.5*(Fwd[2][i] + Fwd[3][i])*nx;
-
-                numflux[3][i] = 0.5*(Fwd[2][i] + Fwd[3][i])*nx;
-                
-                numflux[4][i] = (sqrt6*Fwd[0][i] + 3*sqrt2*Fwd[1][i]
-                                 + 2*sqrt3*Fwd[4][i])*nx/6.0 ;
-
-                numflux[5][i] = 0.0;
-            }
-            else
-            {
-                numflux[0][i] = (-sqrt3*Bwd[0][i] + 3*Bwd[1][i]
-                                 - sqrt6*Bwd[4][i])*nx/6.0 ;
-
-                numflux[1][i] = 0.5*(Bwd[0][i] - sqrt3*Bwd[1][i]
-                                     + sqrt2*Bwd[4][i])*nx;
-                
-                numflux[2][i] = 0.5*(-Bwd[2][i] + Bwd[3][i])*nx;
-
-                numflux[3][i] = 0.5*(Bwd[2][i] - Bwd[3][i])*nx;
-                
-                numflux[4][i] = (-sqrt6*Bwd[0][i] + 3*sqrt2*Bwd[1][i]
-                                 - 2*sqrt3*Bwd[4][i])*nx/6.0 ;
-
-                numflux[5][i] = 0.0;
-                
-            }
-
             NekDouble ny = m_traceNormals[1][i];
-            if(ny > 0)
-            {
-                numflux[0][i] += (sqrt3*Fwd[0][i] + 3*Fwd[2][i]
-                                 + sqrt6*Fwd[5][i])*ny/6.0 ;
+	    // Interior matrix
+            //[ -3^(1/2)/6, nx/2, ny/2, -(3^(1/2)*nx*ny)/3, -(6^(1/2)*nx^2)/6, -(6^(1/2)*ny^2)/6]
+            Interior[0][0] = -sqrt(3.0)/6.;          
+            Interior[0][1] =  nx/2.;              
+            Interior[0][2] =  ny/2.; 
+            Interior[0][3] = -sqrt(3.0)*nx*ny/3.0;   
+            Interior[0][4] = -sqrt(6.)*nx*nx/6.0; 
+            Interior[0][5] = -sqrt(6.)*ny*ny/6.0;
+	    //[ nx/2, -(3^(1/2)*nx^2)/2 - ny^2/2, -(nx*ny*(3^(1/2) - 1))/2, ny/6, (2^(1/2)*nx)/6, 0]
+            Interior[1][0] =  nx/2.;  
+            Interior[1][1] = -sqrt(3.)*nx*nx/2. - ny*ny/2;  
+            Interior[1][2] = -(nx*ny*(sqrt(3.) - 1))/2; 
+            Interior[1][3] =  ny/6.; 
+            Interior[1][4] =  sqrt(2.)*nx/6.0;               
+            Interior[1][5] =  0;
+            //[ ny/2, -(nx*ny*(3^(1/2) - 1))/2, -nx^2/2 - (3^(1/2)*ny^2)/2, nx/6, 0, (2^(1/2)*ny)/6]
+            Interior[2][0] = ny/2.;  
+            Interior[2][1] = -(nx*ny*(sqrt(3.) - 1))/2.;  
+            Interior[2][2] = -nx*nx/2.-(sqrt(3.)*ny*ny)/2.;    
+            Interior[2][3] = nx/6.0; 
+            Interior[2][4] = 0;                           
+            Interior[2][5] = sqrt(2.)*ny/6.0;
+            // [ -(3^(1/2)*nx*ny)/3, ny/6, nx/6, - (nx^2 - ny^2)^2/2 - (2*3^(1/2)*nx^2*ny^2)/3, -(2^(1/2)*nx*ny*(2*3^(1/2)*nx^2 - 3*nx^2 + 3*ny^2))/6, -(2^(1/2)*nx*ny*(2*3^(1/2)*ny^2 + 3*nx^2 - 3*ny^2))/6]
+            Interior[3][0] = -(sqrt(3.)*nx*ny)/3.;  
+            Interior[3][1] = ny/6.;  
+            Interior[3][2] = nx/6.;   
+            Interior[3][3] = -(nx*nx-ny*ny)*(nx*nx-ny*ny)/2.-(2*sqrt(3.)*nx*nx*ny*ny)/3.; 
+            Interior[3][4] = -(sqrt(2.)*nx*ny*(2*sqrt(3.)*nx*nx-3*nx*nx+3*ny*ny))/6.;
+            Interior[3][5] = -(sqrt(2.)*nx*ny*(2*sqrt(3.)*ny*ny+3*nx*nx-3*ny*ny))/6.;
+            // [ -(6^(1/2)*nx^2)/6, (2^(1/2)*nx)/6, 0, -(2^(1/2)*nx*ny*(2*3^(1/2)*nx^2 - 3*nx^2 + 3*ny^2))/6, - (3^(1/2)*nx^4)/3 - nx^2*ny^2, -(nx^2*ny^2*(3^(1/2) - 3))/3]
+            Interior[4][0] = -(sqrt(6.)*nx*nx)/6.;
+            Interior[4][1] =  (sqrt(2.)*nx)/6.;
+            Interior[4][2] =  0;
+            Interior[4][3] = -(sqrt(2.)*nx*ny*(2*sqrt(3.)*nx*nx-3*nx*nx+3*ny*ny))/6.;
+            Interior[4][4] = -(sqrt(3.)*nx*nx*nx*nx)/3.-nx*nx*ny*ny;
+            Interior[4][5] = -(nx*nx*ny*ny*(sqrt(3.)-3))/3.;
+            // [ -(6^(1/2)*ny^2)/6, 0, (2^(1/2)*ny)/6, -(2^(1/2)*nx*ny*(2*3^(1/2)*ny^2 + 3*nx^2 - 3*ny^2))/6, -(nx^2*ny^2*(3^(1/2) - 3))/3, - nx^2*ny^2 - (3^(1/2)*ny^4)/3]
+            Interior[5][0] = -(sqrt(6.)*ny*ny)/6.;
+            Interior[5][1] =  0;
+            Interior[5][2] =  (sqrt(2.)*ny)/6.;
+            Interior[5][3] = -(sqrt(2.)*nx*ny*(2*sqrt(3.)*ny*ny+3*nx*nx-3*ny*ny))/6.;
+            Interior[5][4] = -(nx*nx*ny*ny*(sqrt(3.)-3.))/3.;
+            Interior[5][5] = - nx*nx*ny*ny-(sqrt(3)*ny*ny*ny*ny)/3.;
 
-                numflux[1][i] += 0.5*(Fwd[1][i] + Fwd[3][i])*ny;
+       
+            // Exterior matrix
+            // [ 3^(1/2)/6, nx/2, ny/2, (3^(1/2)*nx*ny)/3, (6^(1/2)*nx^2)/6, (6^(1/2)*ny^2)/6]
+            Exterior[0][0] = sqrt(3.)/6.;   
+            Exterior[0][1] = nx/2.; 
+	    Exterior[0][2] = ny/2.;
+	    Exterior[0][3] = sqrt(3.)*nx*ny/3.0;
+	    Exterior[0][4] = sqrt(6.)*nx*nx/6.0;
+	    Exterior[0][5] = sqrt(6.)*ny*ny/6.0;
+            // [ nx/2, (3^(1/2)*nx^2)/2 + ny^2/2, (nx*ny*(3^(1/2) - 1))/2, ny/6, (2^(1/2)*nx)/6, 0]
+            Exterior[1][0] = nx/2.;
+	    Exterior[1][1] = sqrt(3.)*nx*nx/2.0+ny*ny/2;
+	    Exterior[1][2] = (nx*ny*(sqrt(3.) - 1))/2;
+	    Exterior[1][3] = ny/6.0;
+	    Exterior[1][4] = sqrt(2.)*nx/6.0;
+	    Exterior[1][5] = 0;
+            // [ ny/2, (nx*ny*(3^(1/2) - 1))/2, nx^2/2 + (3^(1/2)*ny^2)/2, nx/6, 0, (2^(1/2)*ny)/6]
+            Exterior[2][0] = ny/2.;
+	    Exterior[2][1] = (nx*ny*(sqrt(3.)-1))/2.;
+	    Exterior[2][2] = nx*nx/2.-(sqrt(3.)*ny*ny)/2.;
+	    Exterior[2][3] = nx/6.;
+	    Exterior[2][4] = 0;
+	    Exterior[2][5] = sqrt(2.)*ny/6.;
+            // [ (3^(1/2)*nx*ny)/3, ny/6, nx/6, (nx^2 - ny^2)^2/2 + (2*3^(1/2)*nx^2*ny^2)/3, (2^(1/2)*nx*ny*(2*3^(1/2)*nx^2 - 3*nx^2 + 3*ny^2))/6, (2^(1/2)*nx*ny*(2*3^(1/2)*ny^2 + 3*nx^2 - 3*ny^2))/6]
+ 	    Exterior[3][0] = (sqrt(3.)*nx*ny)/3.; 
+	    Exterior[3][1] = ny/6.;
+	    Exterior[3][2] = nx/6.;
+	    Exterior[3][3] = (nx*nx-ny*ny)*(nx*nx-ny*ny)/2.-(2*sqrt(3.)*nx*nx*ny*ny)/3.;
+	    Exterior[3][4] = (sqrt(2.)*nx*ny*(2*sqrt(3.)*nx*nx-3*nx*nx+3*ny*ny))/6.;
+	    Exterior[3][5] = (sqrt(2.)*nx*ny*(2*sqrt(3.)*ny*ny+3*nx*nx-3*ny*ny))/6.;
+            // [ (6^(1/2)*nx^2)/6, (2^(1/2)*nx)/6, 0, (2^(1/2)*nx*ny*(2*3^(1/2)*nx^2 - 3*nx^2 + 3*ny^2))/6, (3^(1/2)*nx^4)/3 + nx^2*ny^2, (nx^2*ny^2*(3^(1/2) - 3))/3]
+            Exterior[4][0] = (sqrt(6.)*nx*nx)/6.;
+	    Exterior[4][1] = (sqrt(2.)*nx)/6.;
+	    Exterior[4][2] = 0; 
+	    Exterior[4][3] = (sqrt(2.)*nx*ny*(2*sqrt(3.)*nx*nx-3*nx*nx+3*ny*ny))/6.;
+	    Exterior[4][4] = (sqrt(3.)*nx*nx*nx*nx)/3.+nx*nx*ny*ny;
+	    Exterior[4][5] = (nx*nx*ny*ny*(sqrt(3.)-3))/3.;
+            // [ (6^(1/2)*ny^2)/6, 0, (2^(1/2)*ny)/6, (2^(1/2)*nx*ny*(2*3^(1/2)*ny^2 + 3*nx^2 - 3*ny^2))/6, (nx^2*ny^2*(3^(1/2) - 3))/3, nx^2*ny^2 + (3^(1/2)*ny^4)/3]
+            Exterior[5][0] = (sqrt(6.)*ny*ny)/6.;
+	    Exterior[5][1] = 0;
+	    Exterior[5][2] = (sqrt(2.)*ny)/6.;
+	    Exterior[5][3] = (sqrt(2.)*nx*ny*(2*sqrt(3.)*ny*ny+3*nx*nx-3*ny*ny))/6.;
+	    Exterior[5][4] = (nx*nx*ny*ny*(sqrt(3.)-3.))/3.;
+	    Exterior[5][5] =  nx*nx*ny*ny+(sqrt(3)*ny*ny*ny*ny)/3.;
 
-                numflux[2][i] += 0.5*(Fwd[0][i] + sqrt3*Fwd[2][i]
-                                     + sqrt2*Fwd[5][i])*ny;
+       
+            // Fwd exter
+            // Bwd inter
 
-                numflux[3][i] += 0.5*(Fwd[1][i] + Fwd[3][i])*ny;
-                
-                numflux[5][i] += (sqrt6*Fwd[0][i] + 3*sqrt2*Fwd[2][i]
-                                 + 2*sqrt3*Fwd[5][i])*ny/6.0 ;
-
-            }
-            else
-            {
-                numflux[0][i] += (-sqrt3*Bwd[0][i] + 3*Bwd[2][i]
-                                 - sqrt6*Bwd[5][i])*ny/6.0 ;
-
-                numflux[1][i] += 0.5*(-Bwd[1][i] + Bwd[3][i])*ny;
-
-                numflux[2][i] += 0.5*(Bwd[0][i] - sqrt3*Bwd[2][i]
-                                     + sqrt2*Bwd[5][i])*ny;
-
-                numflux[3][i] += 0.5*(Bwd[1][i] - Bwd[3][i])*ny;
-                
-                numflux[5][i] += (-sqrt6*Bwd[0][i] + 3*sqrt2*Bwd[2][i]
-                                 - 2*sqrt3*Bwd[5][i])*ny/6.0 ;
-
-            }
-	    cout << "(nx,ny) = " << "("<<nx<<", "<<ny<<")"<<endl;
             for(int j = 0; j < 6; ++j)
             {
-                numflux[j][i] *= m_sqrtRT;
+                 
+		numflux[j][i] = Interior[j][0]*Bwd[0][i]+Interior[j][1]*Bwd[1][i]+Interior[j][2]*Bwd[2][i]+Interior[j][3]*Bwd[3][i]+Interior[j][4]*Bwd[4][i]+Interior[j][5]*Bwd[5][i]+
+           		        Exterior[j][0]*Fwd[0][i]+Exterior[j][1]*Fwd[1][i]+Exterior[j][2]*Fwd[2][i]+Exterior[j][3]*Fwd[3][i]+Exterior[j][4]*Fwd[4][i]+Exterior[j][5]*Fwd[5][i];
+                numflux[j][i] *= (m_sqrtRT);
             }
+            //cout << nx << "\t" << ny <<"\t" <<endl; 
         }
     }
 
     void GalerkinBoltzmann::AddSourceTerms( const Array<OneD, const  Array<OneD, NekDouble> >&inarray,
-                             Array<OneD,        Array<OneD, NekDouble>        >&outarray)
+                                                  Array<OneD,        Array<OneD, NekDouble> >&outarray)
     {
 
         int nq = m_fields[0]->GetTotPoints();
@@ -379,15 +401,22 @@ namespace Nektar
         //-------------------------------------------------
         Array<OneD, NekDouble> force(nq);
         
-        NekDouble invsqrt_2 = 1.0/sqrt(2);
-        NekDouble invtau = -1.0/m_tau;
+        NekDouble invsqrt_2 =  1.0/sqrt(2);
+        NekDouble invtau    = -1.0/m_tau;
+	/*
+	Vmath::Zero(nq, outarray[0], 1);
+	Vmath::Zero(nq, outarray[1], 1);
+	Vmath::Zero(nq, outarray[2], 1);
+	Vmath::Zero(nq, outarray[3], 1);
+	Vmath::Zero(nq, outarray[4], 1);
+	Vmath::Zero(nq, outarray[5], 1);
+	*/
         // outarray[3] = -1/tau (a[3] - a[1] a[2]/a[0])
         Vmath::Vmul(nq,inarray[1],1,inarray[2],1,force,1);
         Vmath::Vdiv(nq,force,1,inarray[0],1,force,1);
         Vmath::Vsub(nq,inarray[3],1,force,1,force,1);
         Vmath::Smul(nq,invtau,force,1,force,1);
         Vmath::Vadd(nq,outarray[3],1,force,1,outarray[3],1);
-
         // outarray[4] = -1/tau (a[4] - 1/sqrt(2) x a[1] a[1]/a[0])
         Vmath::Vmul(nq,inarray[1],1,inarray[1],1,force,1);
         Vmath::Vdiv(nq,force,1,inarray[0],1,force,1);
@@ -395,7 +424,6 @@ namespace Nektar
         Vmath::Vsub(nq,inarray[4],1,force,1,force,1);
         Vmath::Smul(nq,invtau,force,1,force,1);
         Vmath::Vadd(nq,outarray[4],1,force,1,outarray[4],1);
-
         // outarray[5] = -1/tau (a[5] - 1/sqrt(2) x a[2] a[2]/a[0])
         Vmath::Vmul(nq,inarray[2],1,inarray[2],1,force,1);
         Vmath::Vdiv(nq,force,1,inarray[0],1,force,1);
