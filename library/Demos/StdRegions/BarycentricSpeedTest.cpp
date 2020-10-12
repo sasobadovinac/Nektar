3ff4ea2dbb171146333f553586bb3ed6962bb0df
Ed Laughton <ed.laughton@gmail.com>
2020-10-12 15:43:41 +0100

    Precalculate interpolation matrix

--- library/Demos/StdRegions/BarycentricSpeedTest.cpp
+++ library/Demos/StdRegions/BarycentricSpeedTest.cpp
@@ -22,6 +22,42 @@ Array<OneD, NekDouble> EvalPoly(const Array<OneD, const Array<OneD, NekDouble>> 
     return ret;
 }
 
+// derivative in x = 2x
+Array<OneD, NekDouble> EvalPolyDerivx(const Array<OneD, const Array<OneD, NekDouble> > &pts)
+{
+    Array<OneD, NekDouble> ret(pts[0].size());
+    for (int i = 0; i < pts[0].size(); i++)
+    {
+        ret[i] =  2*pts[0][i];
+    }
+
+    return ret;
+}
+
+// derivative in y = 2y
+Array<OneD, NekDouble> EvalPolyDerivy(const Array<OneD, const Array<OneD, NekDouble> > &pts)
+{
+    Array<OneD, NekDouble> ret(pts[0].size());
+    for (int i = 0; i < pts[0].size(); i++)
+    {
+        ret[i] =  2*pts[1][i];
+    }
+
+    return ret;
+}
+
+// derivative in z = -2z
+Array<OneD, NekDouble> EvalPolyDerivz(const Array<OneD, const Array<OneD, NekDouble> > &pts)
+{
+    Array<OneD, NekDouble> ret(pts[0].size());
+    for (int i = 0; i < pts[0].size(); i++)
+    {
+        ret[i] =  -2*pts[2][i] ;
+    }
+
+    return ret;
+}
+
 int main(int argc, char *argv[])
 {
     DemoSupport demo;
@@ -53,64 +89,115 @@ int main(int argc, char *argv[])
         coordIn[i] = Array<OneD, NekDouble>(dimension, 0.0);
         for (int j =0; j < dimension; ++j)
         {
-            coordIn[i][j] = coordsE[j][i];
+            coordIn[i][j] = coordsF[j][i];
         }
     }
-
 
     // Do tests for 1000 cycles
     int totCyc = 1000;
     // Calc interpolation matrix every call
     LibUtilities::Timer t;
 
-    NekDouble phys, deriv0, deriv1, deriv2;
+    Array<OneD, NekDouble> Ephys(totPoints), Ederiv0(totPoints), Ederiv1(totPoints), Ederiv2(totPoints);
+    Array<OneD, NekDouble> Sphys(totPoints), Sderiv0(totPoints), Sderiv1(totPoints), Sderiv2(totPoints);
+    Array<OneD, NekDouble> Bphys(totPoints), Bderiv0(totPoints), Bderiv1(totPoints), Bderiv2(totPoints);
 
-    Array<OneD, NekDouble> physDeriv0(totPoints), physDeriv1(totPoints), physDeriv2(totPoints);
-    std::cout << "Testing old method, calc deriv matrix every cycle" << std::endl;
+
+    Array<OneD, NekDouble> EphysDeriv0(totPoints), EphysDeriv1(totPoints), EphysDeriv2(totPoints);
+    std::cout << "Testing old method, calc interp matrix every cycle" << std::endl;
     t.Start();
     for (int cyc = 0; cyc < totCyc; ++cyc)
     {
-        E->PhysDeriv(physIn, physDeriv0, physDeriv1, physDeriv2);
+        E->PhysDeriv(physIn, EphysDeriv0, EphysDeriv1, EphysDeriv2);
         for (int i = 0; i < totPoints; ++i)
         {
-            phys   = E->PhysEvaluateOld(coordIn[i], physIn);
-            deriv0 = E->PhysEvaluateOld(coordIn[i], physDeriv0);
-            deriv1 = E->PhysEvaluateOld(coordIn[i], physDeriv1);
-            deriv2 = E->PhysEvaluateOld(coordIn[i], physDeriv1);
+            Ephys[i]   = E->PhysEvaluateOld(coordIn[i], physIn);
+            Ederiv0[i] = E->PhysEvaluateOld(coordIn[i], EphysDeriv0);
+            Ederiv1[i] = E->PhysEvaluateOld(coordIn[i], EphysDeriv1);
+            Ederiv2[i] = E->PhysEvaluateOld(coordIn[i], EphysDeriv2);
         }
     }
     t.Stop();
     std::cout << "Old method: " << t.TimePerTest(totCyc) << " per cycle (" << totCyc << " cycles)." << std::endl;
 
-    std::cout << "Testing old method, store deriv matrix" << std::endl;
+    Array<OneD, NekDouble> SphysDeriv0(totPoints), SphysDeriv1(totPoints), SphysDeriv2(totPoints);
+    std::cout << "Testing old method, precalc interp matrix" << std::endl;
     t.Start();
-    E->PhysDeriv(physIn, physDeriv0, physDeriv1, physDeriv2);
+    Array<OneD, Array<OneD, DNekMatSharedPtr>>  I(totPoints);
+    for (int i = 0; i < totPoints; ++i)
+    {
+        // Obtain local collapsed corodinate from
+        // cartesian coordinate.
+        Array<OneD, NekDouble> eta = Array<OneD, NekDouble>(3);
+        E->LocCoordToLocCollapsed(coordIn[i],eta);
+
+        // Get Lagrange interpolants.
+        I[i] = Array<OneD, DNekMatSharedPtr>(3);
+        I[i][0] = E->GetBase()[0]->GetI(eta);
+        I[i][1] = E->GetBase()[1]->GetI(eta+1);
+        I[i][2] = E->GetBase()[2]->GetI(eta+2);
+    }
+
     for (int cyc = 0; cyc < totCyc; ++cyc)
     {
+        E->PhysDeriv(physIn, SphysDeriv0, SphysDeriv1, SphysDeriv2);
         for (int i = 0; i < totPoints; ++i)
         {
-            phys   = E->PhysEvaluateOld(coordIn[i], physIn);
-            deriv0 = E->PhysEvaluateOld(coordIn[i], physDeriv0);
-            deriv1 = E->PhysEvaluateOld(coordIn[i], physDeriv1);
-            deriv2 = E->PhysEvaluateOld(coordIn[i], physDeriv1);
+            Sphys[i]   = E->PhysEvaluate(I[i], physIn);
+            Sderiv0[i] = E->PhysEvaluate(I[i], SphysDeriv0);
+            Sderiv1[i] = E->PhysEvaluate(I[i], SphysDeriv1);
+            Sderiv2[i] = E->PhysEvaluate(I[i], SphysDeriv2);
         }
     }
     t.Stop();
     std::cout << "Old method: " << t.TimePerTest(totCyc) << " per cycle (" << totCyc << " cycles)." << std::endl;
 
-
+    Array<OneD, NekDouble> BphysDeriv0(totPoints), BphysDeriv1(totPoints), BphysDeriv2(totPoints);
     std::cout << "Testing barycentric method" << std::endl;
     t.Start();
     for (int cyc = 0; cyc < totCyc; ++cyc)
     {
         for (int i = 0; i < totPoints; ++i)
         {
-            phys = E->PhysEvaluate(coordIn[i], physIn, deriv0, deriv1, deriv2);
+            Bphys[i] = E->PhysEvaluate(coordIn[i], physIn, Bderiv0[i], Bderiv1[i], Bderiv2[i]);
         }
     }
     t.Stop();
     std::cout << "New method: " << t.TimePerTest(totCyc) << " per cycle (" << totCyc << " cycles)." << std::endl;
 
+
+    Array<OneD, NekDouble> sol(totPoints), sol0(totPoints), sol1(totPoints), sol2(totPoints);
+    switch(dimension)
+    {
+        case 3:
+            sol2 = EvalPolyDerivz(coordsF);
+        case 2:
+            sol1 = EvalPolyDerivy(coordsF);
+        case 1:
+            sol0 = EvalPolyDerivx(coordsF);
+        default:
+            sol = EvalPoly(coordsF);
+            break;
+    }
+
+    //Check error
+    std::cout << "\nBarycentric \t\t L2 Error \t Linf Error" << std::endl;
+    std::cout << "\tPhys: "   << "\t\t" << F->L2(Bphys, sol) << "\t" << F->Linf(Bphys, sol)   << std::endl;
+    std::cout << "\tDeriv0: " << "\t" << F->L2(Bderiv0, sol0) << "\t" << F->Linf(Bderiv0, sol0)   << std::endl;
+    std::cout << "\tDeriv1: " << "\t" << F->L2(Bderiv1, sol1) << "\t" << F->Linf(Bderiv1, sol1)   << std::endl;
+    std::cout << "\tDeriv2: " << "\t" << F->L2(Bderiv2, sol2) << "\t" << F->Linf(Bderiv2, sol2)   << std::endl;
+
+    std::cout << "\nStore matrix \t\t L2 Error \t Linf Error" << std::endl;
+    std::cout << "\tPhys: "   << "\t\t" << F->L2(Sphys, sol) << "\t" << F->Linf(Sphys, sol)   << std::endl;
+    std::cout << "\tDeriv0: " << "\t" << F->L2(Sderiv0, sol0) << "\t" << F->Linf(Sderiv0, sol0)   << std::endl;
+    std::cout << "\tDeriv1: " << "\t" << F->L2(Sderiv1, sol1) << "\t" << F->Linf(Sderiv1, sol1)   << std::endl;
+    std::cout << "\tDeriv2: " << "\t" << F->L2(Sderiv2, sol2) << "\t" << F->Linf(Sderiv2, sol2)   << std::endl;
+
+    std::cout << "\nCalc every cycle \t L2 Error \t Linf Error" << std::endl;
+    std::cout << "\tPhys: "   << "\t\t" << F->L2(Ephys, sol) << "\t" << F->Linf(Ephys, sol)   << std::endl;
+    std::cout << "\tDeriv0: " << "\t" << F->L2(Ederiv0, sol0) << "\t" << F->Linf(Ederiv0, sol0)   << std::endl;
+    std::cout << "\tDeriv1: " << "\t" << F->L2(Ederiv1, sol1) << "\t" << F->Linf(Ederiv1, sol1)   << std::endl;
+    std::cout << "\tDeriv2: " << "\t" << F->L2(Ederiv2, sol2) << "\t" << F->Linf(Ederiv2, sol2)   << std::endl;
 
     // Iterative calc interpolation matrix
 
