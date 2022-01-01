158047452b3164cace6f8664c42a8f1b53307ace
ssherw <s.sherwin@imperial.ac.uk>
2022-01-01 23:07:03 +0000

    Added missing double quotation

--- solvers/CompressibleFlowSolver/Diffusion/DiffusionLDGNS.cpp
+++ solvers/CompressibleFlowSolver/Diffusion/DiffusionLDGNS.cpp
@@ -44,7 +44,7 @@
 namespace Nektar
 {
 std::string DiffusionLDGNS::type = GetDiffusionFactory().
-    RegisterCreatorFunction("LDGNS", DiffusionLDGNS::create, "LDG for Navier-Stokes);
+    RegisterCreatorFunction("LDGNS", DiffusionLDGNS::create, "LDG for Navier-Stokes");
 
 DiffusionLDGNS::DiffusionLDGNS()
 {
