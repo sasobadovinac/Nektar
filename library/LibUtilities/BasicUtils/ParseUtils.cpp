6c530f6b3a72e452277b7e1e488374469a0e49ea
Sherwin <ssherw@ic-t3697hj6kr.home>
2022-07-10 14:53:15 +0100

    Update following compiler errors on Monterey system

--- library/LibUtilities/BasicUtils/ParseUtils.cpp
+++ library/LibUtilities/BasicUtils/ParseUtils.cpp
@@ -77,8 +77,6 @@ struct PushBackFunctor
         }
     }
 private:
-    // Do not allow assignment
-    PushBackFunctor& operator=(const PushBackFunctor& src) = delete;
 
     /// Storage vector that will hold parsed variables from boost::spirit.
     std::vector<T> &m_vec;
