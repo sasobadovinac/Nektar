7425fd185bdd9776064e891f2bfc869524cdacce
Spencer Sherwin <s.sherwin@imperial.ac.uk>
2022-07-22 22:37:46 +0100

    removed erroneous iterperexp call

--- library/MultiRegions/Python/ExpList.cpp
+++ library/MultiRegions/Python/ExpList.cpp
@@ -270,7 +270,7 @@ void ExpList_LoadField(ExpListSharedPtr exp, std::string filename, std::string v
         }
     }
 
-    exp->BwdTrans_IterPerExp(exp->GetCoeffs(), exp->UpdatePhys());
+    exp->BwdTrans(exp->GetCoeffs(), exp->UpdatePhys());
 }
 
 void export_ExpList()
